ca3999e10f7cbe0add80e263dc4959a75b346f40
sybao <luongsybao2505@gmail.com>
2022-11-07 12:53:38 +0700

    css fix

--- src/components/Model.js
+++ src/components/Model.js
@@ -4,13 +4,17 @@ import styleModel from "./Model.module.css";
 
 export default class Modal extends Component {
   render() {
-    const { url } = this.props.selectedItem;
+    const { name, url, desc } = this.props.selectedItem;
     return (
       <div className={styleModel.item}>
         <img className="rounded mx-auto d-block" src={ModelImg} alt="" />
 
         <div>
           <img className={styleModel.itemImg} src={url} alt="" />
+        </div>
+        <div className={styleModel.title}>
+          <h3 style={{ color: "blueviolet" }}>{name}</h3>
+          <p className="fw-bolder text-white">{desc}</p>
         </div>
       </div>
     );
